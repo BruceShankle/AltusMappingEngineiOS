9f35e256a8f3e50aae8cebe92a2aa976bd52018b
Bruce Shankle <bruce@ba3.us>
2012-12-10 17:14:43 -0500

    Add GPS support and track-up mode.

---
+++ MyMap/MyCLController.m
@@ -0,0 +1,13 @@
+//
+//  MyCLController.m
+//  MyMap
+//
+//  Created by Bruce Shankle III on 12/10/12.
+//  Copyright (c) 2012 BA3, LLC. All rights reserved.
+//
+
+#import "MyCLController.h"
+
+@implementation MyCLController
+
+@end
