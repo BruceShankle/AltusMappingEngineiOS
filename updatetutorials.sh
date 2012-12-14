e362b2d876bddbc90b34148703ef7f5c8dd878e3
Bruce Shankle <bruce@ba3.us>
2012-12-14 05:03:36 -0500

    Adding script which can merge blankslate down to all the tutorial branches.

---
+++ updatetutorials.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#The intent of this script is to update all of the
+#tutorial branches with updates from blankslate.
+git checkout tutorial1
+git merge blankslate -m 'Updating engine.'
+
