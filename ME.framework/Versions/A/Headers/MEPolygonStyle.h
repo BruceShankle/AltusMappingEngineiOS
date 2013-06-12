de3bba51c815809aef305af2396a5be27c498fdc
Bruce Shankle <bruce@ba3.us>
2013-06-12 14:10:27 -0700

    Update mapping engine binaries with new build to support WMS.

--- ME.framework/Versions/A/Headers/MEPolygonStyle.h
+++ ME.framework/Versions/A/Headers/MEPolygonStyle.h
@@ -10,10 +10,21 @@
 /**Color to fill object with.*/
 @property (atomic, retain) UIColor* fillColor;
 
+/**Texture to use to fill the polygon.*/
+@property (atomic, retain) NSString* textureName;
+
+/**Color for drop shadow.*/
+@property (atomic, retain) UIColor* shadowColor;
+
 /**Init with stroke color, stroke width, and fill color.*/
 - (id) initWithStrokeColor:(UIColor*) color
                strokeWidth:(CGFloat) width
                  fillColor:(UIColor*) fcolor;
+
+/**Init with stroke color, stroke width, and texture.*/
+- (id) initWithStrokeColor:(UIColor*) color
+               strokeWidth:(CGFloat) width
+               textureName:(NSString*) textureName;
 
 /**Inits with the specified fill color, clear stroke color and stroke width of 1.*/
 - (id) initWithFillColor:(UIColor*) fColor;
