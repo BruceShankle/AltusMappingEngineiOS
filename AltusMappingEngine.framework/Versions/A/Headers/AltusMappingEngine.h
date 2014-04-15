b51cc8d81dd94edc60682656b10dffb27e2c1a51
Bruce Shankle <bruce@ba3.us>
2014-04-15 00:43:16 -0400

    Shield against invalid viewport that occurs when application controls timing and creates MEMapViewController instances and does not call update before gesture recognizers receive events.

---
+++ AltusMappingEngine.framework/Versions/A/Headers/AltusMappingEngine.h
@@ -0,0 +1,27 @@
+//  Copyright (c) 2014 BA3, LLC. All rights reserved.
+#pragma once
+#import <AltusMappingEngine/DirectoryInfo.h>
+#import <AltusMappingEngine/MELineStyle.h>
+#import <AltusMappingEngine/MEPolygonStyle.h>
+#import <AltusMappingEngine/MEAnimatedVectorCircle.h>
+#import <AltusMappingEngine/MEAnimatedVectorReticle.h>
+#import <AltusMappingEngine/MEGeographicBounds.h>
+#import <AltusMappingEngine/METileInfo.h>
+#import <AltusMappingEngine/METileProvider.h>
+#import <AltusMappingEngine/MEMath.h>
+#import <AltusMappingEngine/MEColorBar.h>
+#import <AltusMappingEngine/MEMapView.h>
+#import <AltusMappingEngine/MEMapViewController.h>
+#import <AltusMappingEngine/MERenderModes.h>
+#import <AltusMappingEngine/MEGeometry.h>
+#import <AltusMappingEngine/MEProtocols.h>
+#import <AltusMappingEngine/MEMarkerInfo.h>
+#import <AltusMappingEngine/MEImageUtil.h>
+#import <AltusMappingEngine/MEFontUtil.h>
+#import <AltusMappingEngine/MEMapInfo.h>
+#import <AltusMappingEngine/MEHitTesting.h>
+#import <AltusMappingEngine/MEGeometryGroup.h>
+#import <AltusMappingEngine/Enums.h>
+#import <AltusMappingEngine/MEHeightProfiling.h>
+#import <AltusMappingEngine/MEMarker.h>
+#import <AltusMappingEngine/MEMarkerQuery.h>
